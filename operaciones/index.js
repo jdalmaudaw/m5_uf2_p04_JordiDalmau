74b6d218803793900c58e843bc57dd7ed787d30a
DAlmauDAW <jordidalmau.alum@insestatut.cat>
2024-02-15 19:59:45 +0100

    v1.2 suma

--- operaciones/index.js
+++ operaciones/index.js
@@ -1,23 +1,23 @@
 /**
  * Aquesta funcio suma tots els valors passats per parametre
- * 
+ *
  * @param  {...Number} valors els valors amb els que realitzar la operacio
  * @returns {Number} el resultat de la operacio
  */
-function suma(...valors) {
-
+function suma(...valors) {}
+let resultat = 0;
+for (const valor of valors) {
+  resultat += valor;
 }
-
+return resultat;
 
 /**
  * Aquesta funcio multiplica tots els valors passats per parametre
- * 
+ *
  * @param  {...Number} valors els valors amb els que realitzar la operacio
  * @returns {Number} el resultat de la operacio
  */
-function multiplica(...valors) {
-
-}
+function multiplica(...valors) {}
 
 module.exports.suma = suma;
 module.exports.multiplica = multiplica;
